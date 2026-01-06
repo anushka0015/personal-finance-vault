91ea47e7d900ffc90a8f4ef6f97836a50b254ee0
Anushka Joshi <anushkajoshi15032006@gmail.com>
2026-01-06 15:55:34 +0530

    add signup page UI

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to Personal Finance Vault")
-
+def signup(request):
+    return render(request, 'users/signup.html')
 
 # Create your views here.
